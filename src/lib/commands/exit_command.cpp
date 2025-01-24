dba9fc9b43e961aff7e1f2dca272b20b65ce5329
Alexander Nalobin <m_troc@mail.ru>
2025-01-24 22:48:45 +0300

    Move implementation of exit_command from .h to .cpp file

--- src/lib/commands/exit_command.cpp
+++ src/lib/commands/exit_command.cpp
@@ -5,4 +5,10 @@
 #include <iostream>
 #include <vector>
 
-namespace console {}  // namespace console
+namespace console {
+int ExitCommand::execute(const std::vector<std::string>& args) {
+    (void)args;
+    shell_.stop();
+    return 0;
+};
+}  // namespace console
--- src/lib/include/commands/exit_command.h
+++ src/lib/include/commands/exit_command.h
@@ -8,11 +8,7 @@ namespace console {
 class ExitCommand : public Command {
    public:
     explicit ExitCommand(Shell& shell) : shell_(shell) {};
-    int execute(const std::vector<std::string>& args) override {
-        (void)args;
-        shell_.stop();
-        return 0;
-    };
+    int execute(const std::vector<std::string>& args) override ;
 
    private:
     Shell& shell_;
